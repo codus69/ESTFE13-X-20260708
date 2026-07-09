470eef6ddca78caba045bc554053b2f28a6c91aa
이채연 <196144872+codus69@users.noreply.github.com>
2026-07-09 15:15:50 +0900

    feat: 컴포넌트, 로그인 시 상단메뉴 출력, 로그아웃 추가

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router';
-import { Box, Button } from '@mui/material';
+import { Link } from "react-router";
+import { Box, Button } from "@mui/material";
+
 
 const Nav = () => {
   return (
-    <Box component="nav" sx={{ mt: 2, display: 'flex', gap: 2 }}>
+    <Box component="nav" sx={{  mt: 2, display: "flex", gap: 2 }}>
       <Button component={Link} to="/" variant="text">
         Home
       </Button>
+
 
       <Button component={Link} to="/profile" variant="text">
         Profile
@@ -14,5 +16,62 @@ const Nav = () => {
     </Box>
   );
 };
+
+
+export default Nav;
+
+
+
+
+import { Link } from "react-router";
+import { Box, Button } from "@mui/material";
+
+
+const Nav = () => {
+  return (
+    <Box component="nav" sx={{  mt: 2, display: "flex", gap: 2 }}>
+      <Button component={Link} to="/" variant="text">
+        Home
+      </Button>
+
+
+      <Button component={Link} to="/profile" variant="text">
+        Profile
+      </Button>
+    </Box>
+  );
+};
+
+
+export default Nav;
+
+
+
+
+import { Link } from "react-router";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+
+
+const Nav = () => {
+  return (
+    <nav>
+      <List sx={{ display: "flex", gap: 2 }}>
+        <ListItem disablePadding sx={{ width: "auto" }}>
+          <ListItemButton component={Link} to="/">
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+
+
+        <ListItem disablePadding sx={{ width: "auto" }}>
+          <ListItemButton component={Link} to="/profile">
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </nav>
+  );
+};
+
 
 export default Nav;
